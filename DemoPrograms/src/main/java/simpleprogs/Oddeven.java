ef582ba6980e6ade9ecf0a7175e4b610664e4296
MIDHUN GM <MIDHUN GM@LAPTOP-QFRVP6L9>
2023-08-16 12:58:03 +0530

    Statement update

--- DemoPrograms/src/main/java/simpleprogs/Oddeven.java
+++ DemoPrograms/src/main/java/simpleprogs/Oddeven.java
@@ -1,19 +1,20 @@
 package simpleprogs;
-import java.util.Scanner;
+//import java.util.Scanner;
 public class Oddeven 
 {
 	public static void main(String[] args) 
 	{
-		System.out.println("Enter a number ");
-		Scanner num= new Scanner(System.in);
-		int a= num.nextInt();
+		//System.out.println("Enter a number ");
+		//Scanner num= new Scanner(System.in);
+		//int a= num.nextInt();
+		int a=13;
 		if(a%2==0)
 		{
-			System.out.println("Even");
+			System.out.println("Given number is Even");
 	}
 		else
 		{
-			System.out.println("Odd");
+			System.out.println("Given number is Odd");
 		}
 }
 }
